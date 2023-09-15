8fc46c65109f429b6b5cc5d0f4f0e368aa198b51
Ian Weber <iacweber@gmail.com>
2023-09-15 13:13:07 -0700

    Small adjustments.

--- Program.cs
+++ Program.cs
@@ -3,41 +3,43 @@ using System.Numerics;
 
 Console.WriteLine("Hello, sports fans!");
 //testing information for College of Metal Team
-Player orcLino = new Player("Orc Lineman", 50000);
-Player humanLino = new Player("Human Lineman", 50000);
-Player goblinBruiserLino = new Player("Goblin Bruiser Lineman", 45000);
-Position linos = new Position("Linemen", 16, 3);
-linos.players.Add(orcLino);
-linos.players.Add(humanLino);
-linos.players.Add(goblinBruiserLino);
+static void defineTestTeam()
+{
+    Player orcLino = new Player("Orc Lineman", 50000);
+    Player humanLino = new Player("Human Lineman", 50000);
+    Player goblinBruiserLino = new Player("Goblin Bruiser Lineman", 45000);
+    Position linos = new Position("Linemen", 16, 3);
+    linos.players.Add(orcLino);
+    linos.players.Add(humanLino);
+    linos.players.Add(goblinBruiserLino);
 
-Player orcBlitz = new Player("Orc Blitzer", 80000);
-Player humanBlitz = new Player("Human Blitzer", 85000);
-Position blitzers = new Position("Blitzers", 4, 2);
-blitzers.players.Add(orcBlitz);
-blitzers.players.Add(humanBlitz);
+    Player orcBlitz = new Player("Orc Blitzer", 80000);
+    Player humanBlitz = new Player("Human Blitzer", 85000);
+    Position blitzers = new Position("Blitzers", 4, 2);
+    blitzers.players.Add(orcBlitz);
+    blitzers.players.Add(humanBlitz);
 
-Player orcThrow = new Player("Orc Thrower", 65000);
-Position throwers = new Position("Throwers", 2, 1);
-throwers.players.Add(orcThrow);
+    Player orcThrow = new Player("Orc Thrower", 65000);
+    Position throwers = new Position("Throwers", 2, 1);
+    throwers.players.Add(orcThrow);
 
-Player blackOrc = new Player("Black Orc", 90000);
-Player bigUn = new Player("Big Un Blocker", 90000);
-Player bodyguard = new Player("Bodyguard", 90000);
-Player bloodseeker = new Player("Bloodseeker", 110000);
-Position blockers = new Position("Blockers", 6, 4);
-blockers.players.Add(bodyguard);
-blockers.players.Add(bigUn);
-blockers.players.Add(bloodseeker);
-blockers.players.Add(blackOrc);
-Team metal = new Team("College of Metal");
-metal.positions.Add(linos);
-metal.positions.Add(blockers);
-metal.positions.Add(blitzers);
-metal.positions.Add(throwers);
+    Player blackOrc = new Player("Black Orc", 90000);
+    Player bigUn = new Player("Big Un Blocker", 90000);
+    Player bodyguard = new Player("Bodyguard", 90000);
+    Player bloodseeker = new Player("Bloodseeker", 110000);
+    Position blockers = new Position("Blockers", 6, 4);
+    blockers.players.Add(bodyguard);
+    blockers.players.Add(bigUn);
+    blockers.players.Add(bloodseeker);
+    blockers.players.Add(blackOrc);
+    Team metal = new Team("College of Metal", 50000);
+    metal.positions.Add(linos);
+    metal.positions.Add(blockers);
+    metal.positions.Add(blitzers);
+    metal.positions.Add(throwers);
+}
 
-//A Blood/Dungeon Bowl team is comprised of players. For the sake of this program, and for the sake of not getting sued, players consist of two values.
-//A name, and a price.
+//A Blood/Dungeon Bowl team is comprised of players. For the sake of this program, players consist of two values: name and price.
 public class Player { 
     string name;
     BigInteger cost;
@@ -73,23 +75,31 @@ public class Position
     public List<Player> players;
 
 }
-//Each team has a name, for the blood bowl race or college of magic it represents, and a list of positions.
+//Each team has a name, a cost for rerolls, and a list of positions.
 public class Team
 { string name;
+    BigInteger rerollValue;
   public List<Position> positions;
-    public Team (string collegeName)
+    public Team (string collegeName, BigInteger rerollCost)
     {
-        name=(collegeName);
+        name=collegeName;
+        rerollValue= rerollCost;
     }
 }
 
+//A blood bowl league begins with every player filling out their starting roster.
+//Constraints: 11-16 players, 1 million gold starting budget.
 public class roster
 {
+    //Teams in Blood/Dungeon Bowl are chosen from race or wizard college lists, depending on the game.
+    //For the purposes of this program, we list it at the top of the roster for clarity's sake.
+    //All other calculations with it have already been made.
+    Team raceOrCollege;
     //player count must be within 11-16, inclusive
     int playerCount;
-    //BigInteger was chosen possibly incorrectly, but it can be pared down to int later. This must not exceed one million.
+    //BigInteger was chosen possibly incorrectly, but it can possibly be pared down to int later. This must not exceed one million.
     BigInteger value;
-    //up to eight rerolls. Each has a value between 50000 and 70000. All are 50k in Dungeon Bowl, which is where the more interesting problems exist.
+    //zero to eight rerolls. Each has a value between 50000 and 70000.
     int rerolls;
     //A roster is a list of players. At this point, we're looking at fresh, completely unremarkable nameless unnumbered rookies.
     List<Player> players;
